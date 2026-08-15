4123fd6f00c213faacf7842e83ba4ae0dbd8e29b
Bhaskar Gupta <guptabhaskar9897@gmail.com>
2026-08-15 21:51:01 +0530

    Updated  project

---
+++ patterns/1.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>  // stdio for c
+using namespace std; // new
+
+int main(){
+
+    // using for loop / while / do -while loops 
+    // Questions for Patterns and loops
+
+
+
+    // first square pattern
+
+    int n = 4;
+    for(int i =0; i < n; i++){
+        for(int j = 1; j <=n; j++){
+            cout << j;
+        }
+
+
+        cout << endl;
+    }
+
+
+
+
+
+    return 0;
+}
